97fb986d5776642e198c6c9805eb1fe8ab5922ce
Nick Alvarez <nickalvarez189@gmail.com>
2021-03-10 10:18:08 -0800

    JSON implementation

--- src/main/java/com/team23/stim/classes/MainItem.java
+++ src/main/java/com/team23/stim/classes/MainItem.java
@@ -4,9 +4,20 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonPropertyOrder({"name", "sku", "price"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MainItem {
+  @JsonProperty("name")
   String name;
+  @JsonProperty("sku")
   int sku;
+  @JsonProperty("price")
   BigDecimal price;
   int qty;
   List<Integer> subSKUs;
