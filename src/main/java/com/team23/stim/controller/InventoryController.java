06711768d7113c0d94e9eecd92d8b54d03779623
Nick Alvarez <nickalvarez189@gmail.com>
2021-03-10 22:47:18 -0800

    Add CrossOrigin

--- src/main/java/com/team23/stim/controller/InventoryController.java
+++ src/main/java/com/team23/stim/controller/InventoryController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -86,6 +87,7 @@ public class InventoryController {
 
 
 	@ResponseBody
+	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping("/testEndpoint")
 	public String returnStringToYou(HttpSession session)
 	{
