d7a917f805b70e28502c25582f813c9ab2eb555b
Nick Alvarez <48071045+nicky189@users.noreply.github.com>
2021-03-16 13:44:24 -0700

    Actually import the package required for JSONArray

--- src/main/java/com/team23/stim/controller/InventoryController.java
+++ src/main/java/com/team23/stim/controller/InventoryController.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
